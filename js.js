9a7159be8e364c3f8cc64b8b139fdcbd49be5e3e
juliusryan25 <ryanjulius742@gmail.com>
2020-10-15 15:55:11 +0700

    update layout

--- js.js
+++ js.js
@@ -1,9 +1,31 @@
 // Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
 
-$( '.friend-drawer--onhover' ).on( 'click',  function() {
+
+    function openForm1() {
+      document.getElementById("menu").style.display = "block";
+      document.getElementById("open").style.display = "none";
+    }
+
+    function openForm2() {
+      document.getElementById("myForm").style.display = "block";
+      document.getElementById("menu").style.display = "none";
+    }
+
+    function back() {
+      document.getElementById("menu").style.display = "block";
+      document.getElementById("myForm").style.display = "none";
+      document.getElementById("open").style.display = "none";
+    }
+
+    function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+      document.getElementById("open").style.display = "block";
+    }
+
+    function closeForm2() {
+      document.getElementById("menu").style.display = "none";
+      document.getElementById("open").style.display = "block";
+    }
   
-    $( '.chat-bubble' ).hide('slow').show('slow');
-    
-  });
   
   // Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
