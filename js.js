7954ec4812d3397b528533e194d9bf1d3ea2d8c6
juliusryan25 <ryanjulius742@gmail.com>
2020-10-16 14:05:51 +0700

    updatelayout

--- js.js
+++ js.js
@@ -13,6 +13,13 @@
       document.getElementById("menu").style.display = "none";
       document.getElementById("menu").style.animation = "-webkit-animation: fadeInUp 500ms ease-in-out; fadeInUp 500ms ease-in-out;";
     }
+    function openkategori1() {
+      document.getElementById("Kategori1").style.display = "block";
+      document.getElementById("myForm").style.animation = "none";
+      document.getElementById("myForm").style.display = "none";
+      document.getElementById("menu").style.display = "none";
+     
+    }
 
     function back() {
       document.getElementById("menu").style.display = "block";
@@ -30,6 +37,12 @@
 
     function closeForm2() {
       document.getElementById("menu").style.display = "none";
+      document.getElementById("open").style.display = "block";
+      document.getElementById("myForm").style.animation = "none";
+      
+    }
+    function closeFormk1() {
+      document.getElementById("kategori1").style.display = "none";
       document.getElementById("open").style.display = "block";
       document.getElementById("myForm").style.animation = "none";
       
